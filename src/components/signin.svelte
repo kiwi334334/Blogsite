4fffe79fa87d29ef1452617c18e821a9a9507dd6
kiwi334334 <99292273+kiwi334334@users.noreply.github.com>
2023-08-21 23:04:39 +0000

    first commit

---
+++ src/components/signin.svelte
@@ -0,0 +1,12 @@
+<script>
+    let username = ""
+    let password = ""
+</script>
+
+<div class=" bg-stone-600 "> 
+    <input placeholder="type Username here" type="text" bind:value={username}>
+    <input placeholder="type Password here" type="text" bind:value={password}>
+    {#if (username === "EW" && password === "GROSS")}
+        <a href="./main">Go To Main Page</a>
+    {/if}
+</div>
